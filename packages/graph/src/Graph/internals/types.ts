9bd0117d633379343a74a326a18a2a973cff77a2
Michael Ostermann <michaelostermann@me.com>
2026-01-01 12:27:49 +0100

    chore: resolve todos

--- packages/graph/src/Graph/internals/types.ts
+++ packages/graph/src/Graph/internals/types.ts
@@ -1,3 +1,5 @@
+import type { Edge, Node, NodeId, NodeOfType, NodeType } from "../types"
+
 type GetAllTargets<E> = E extends Record<string, infer V>
     ? V extends Record<string, any>
         ? keyof V
@@ -13,4 +15,14 @@ export type Bimap<E> = {
     // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     (K extends keyof E ? E[K] : {})
     & GetReverseMappings<E, K>
+}
+
+export type SourceTypesMap = ReadonlyMap<any, SourceIdsMap>
+export type SourceIdsMap = ReadonlyMap<NodeId, TargetTypesMap>
+export type TargetTypesMap = ReadonlyMap<any, TargetIdsMap>
+export type TargetIdsMap = ReadonlyMap<NodeId, Edge>
+
+export type NodeIdsMap<T extends Node, U extends NodeType<T> = NodeType<T>> = ReadonlyMap<NodeId, NodeOfType<T, U>>
+export interface NodeTypesMap<T extends Node> extends ReadonlyMap<NodeType<T>, NodeIdsMap<T, NodeType<T>>> {
+    get: <V extends NodeType<T>>(type: V) => NodeIdsMap<T, V> | undefined
 }
--- packages/graph/src/Graph/types.ts
+++ packages/graph/src/Graph/types.ts
@@ -1,26 +1,14 @@
-// TODO internal?
+import type { NodeTypesMap, SourceTypesMap } from "./internals/types"
+
 export type NodeId = PropertyKey
 export type Node = { id: NodeId, type: string }
 export type NodeType<T extends Node> = T["type"]
-export type NodeOfType<T extends Node, U extends NodeType<T>> = Extract<T, { type: U }> // TODO this used?
-// TODO U back to extends
-export type NodeIdentifier<T extends Node, U = NodeType<T>> =
+export type NodeOfType<T extends Node, U extends NodeType<T>> = Extract<T, { type: U }>
+export type NodeIdentifier<T extends Node, U extends NodeType<T>> =
     | { id: Extract<T, { type: U }>["id"], type: U }
     | [U, id: Extract<T, { type: U }>["id"]]
 export type Edge = Record<string, unknown> | void
 export type Edges<N extends Node> = Partial<Record<NodeType<N>, Partial<Record<NodeType<N>, Edge>>>>
-
-// TODO internal
-export type SourceTypesMap = ReadonlyMap<any, SourceIdsMap>
-export type SourceIdsMap = ReadonlyMap<NodeId, TargetTypesMap>
-export type TargetTypesMap = ReadonlyMap<any, TargetIdsMap>
-export type TargetIdsMap = ReadonlyMap<NodeId, Edge>
-
-// TODO internal
-export type NodeIdsMap<T extends Node, U extends NodeType<T> = NodeType<T>> = ReadonlyMap<NodeId, NodeOfType<T, U>>
-export interface NodeTypesMap<T extends Node> extends ReadonlyMap<NodeType<T>, NodeIdsMap<T, NodeType<T>>> {
-    get: <V extends NodeType<T>>(type: V) => NodeIdsMap<T, V> | undefined
-}
 
 export interface Graph<N extends Node, _E extends Edges<N>> extends Map<"nodes" | "edges" | "clones", NodeTypesMap<N> | SourceTypesMap | Set<any>> {
     delete: (key: "clones") => boolean
